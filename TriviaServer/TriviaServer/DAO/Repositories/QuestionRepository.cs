9636ec36cbb1ad7ff0a7e97f6651afd84574a6a5
ra049328 <radu.andrei@cerner.com>
2018-10-01 14:54:47 +0300

    improved algorith

--- TriviaServer/TriviaServer/DAO/Repositories/QuestionRepository.cs
+++ TriviaServer/TriviaServer/DAO/Repositories/QuestionRepository.cs
@@ -6,144 +6,140 @@ using System.Threading.Tasks;
 using TriviaServer.DAO.Interfaces;
 using TriviaServer.Models;
 
-namespace TriviaServer.DAO.Repositories 
+namespace TriviaServer.DAO.Repositories
 {
-    public class QuestionRepository : IQuestionRepository
-    {
-        private ApplicationContext _context;
+	public class QuestionRepository : IQuestionRepository
+	{
+		private ApplicationContext _context;
 
-        public QuestionRepository(ApplicationContext context)
-        {
-            _context = context;
-        }
+		public QuestionRepository(ApplicationContext context)
+		{
+			_context = context;
+		}
 
-        public void Create(Question question)
-        {
-            if(_context.Questions.Where(a => a.CategoryId == question.CategoryId)
-                .Where(a => a.QuestionText == question.QuestionText).SingleOrDefault() == null)
-            {
-                _context.Questions.Add(question);
-                _context.SaveChanges();
-            }
-            else
-            {
-                throw new Exception("Question already exists!");
-            }
-        }
+		public void Create(Question question)
+		{
+			if (_context.Questions.Where(a => a.CategoryId == question.CategoryId)
+				.Where(a => a.QuestionText == question.QuestionText).SingleOrDefault() == null)
+			{
+				_context.Questions.Add(question);
+				_context.SaveChanges();
+			}
+			else
+			{
+				throw new Exception("Question already exists!");
+			}
+		}
 
-        public void Edit(Question question)
-        {
-            _context.Questions.Update(question);
-            _context.SaveChanges();
-        }
+		public void Edit(Question question)
+		{
+			_context.Questions.Update(question);
+			_context.SaveChanges();
+		}
 
-        public void Delete(int id)
-        {
-            var question = _context.Questions.SingleOrDefault(x => x.QuestionId == id);
-            if(question != null)
-            {
-                _context.Questions.Remove(question);
-                _context.SaveChanges();
-            }
-            else
-            {
-                throw new Exception("Question not found!");
-            }
-        }
+		public void Delete(int id)
+		{
+			var question = _context.Questions.SingleOrDefault(x => x.QuestionId == id);
+			if (question != null)
+			{
+				_context.Questions.Remove(question);
+				_context.SaveChanges();
+			}
+			else
+			{
+				throw new Exception("Question not found!");
+			}
+		}
 
-        public IEnumerable<Question> GetQuestions()
-        {
-            var questions = _context.Questions.ToList();
-            if(questions.Count > 0)
-            {
-                return questions;
-            }
-            else
-            {
-                throw new Exception("No question was found!");
-            }
-        }
+		public IEnumerable<Question> GetQuestions()
+		{
+			var questions = _context.Questions.ToList();
+			if (questions.Count > 0)
+			{
+				return questions;
+			}
+			else
+			{
+				throw new Exception("No question was found!");
+			}
+		}
 
-        public Question GetByID(int? id)
-        {
-            if (id == null)
-            {
-                return null;
-            }
-            var question = _context.Questions.SingleOrDefault(p => p.QuestionId == id);
-            if(question != null)
-            {
-                return question;
-            }
-            else
-            {
-                throw new Exception("Question not found!");
-            }
-        }
+		public Question GetByID(int? id)
+		{
+			if (id == null)
+			{
+				return null;
+			}
+			var question = _context.Questions.SingleOrDefault(p => p.QuestionId == id);
+			if (question != null)
+			{
+				return question;
+			}
+			else
+			{
+				throw new Exception("Question not found!");
+			}
+		}
 
-        public IEnumerable<Question> GenerateGameQuestions(int uniqueKey)
-        {
-            GameRepository gameRepo = new GameRepository(_context);
-            int gameRoomId = gameRepo.GetGameroomIdByUniqueKey(uniqueKey);
-            var gameCategories = _context.CategoryGames.Where(a => a.GameId == gameRoomId).ToList();
-            List<List<Question>> lowDifficultyQuestions = new List<List<Question>>();
-            List<List<Question>> mediumDifficultyQuestions = new List<List<Question>>();
-            List<List<Question>> highDifficultyQuestions = new List<List<Question>>();
-            List<Question> gameQuestions = new List<Question>();
-            List<Category> categories = new List<Category>();
-            foreach(var gc in gameCategories)
-            {
-                categories.Add(_context.Categories.Where(a => a.CategoryId == gc.CategoryId).FirstOrDefault());
-            }
+		public IEnumerable<Question> GenerateGameQuestions(int uniqueKey)
+		{
+			GameRepository gameRepo = new GameRepository(_context);
+			int gameRoomId = gameRepo.GetGameroomIdByUniqueKey(uniqueKey);
+			var gameCategories = _context.CategoryGames.Include(a => a.Category).Where(a => a.GameId == gameRoomId).Select(a => a.Category).Include(a => a.Questions).ToList();
 
-            foreach (var auxCategory in categories)
-            {
-                lowDifficultyQuestions.Add(_context.Questions.Where(a => a.QuestionDifficulty == 1).Where(a => a.CategoryId == auxCategory.CategoryId).ToList());
-                mediumDifficultyQuestions.Add(_context.Questions.Where(a => a.QuestionDifficulty == 2).Where(a => a.CategoryId == auxCategory.CategoryId).ToList());
-                highDifficultyQuestions.Add(_context.Questions.Where(a => a.QuestionDifficulty == 3).Where(a => a.CategoryId == auxCategory.CategoryId).ToList());
-            }
+			List<Question> lowDifficultyQuestions = new List<Question>();
+			List<Question> mediumDifficultyQuestions = new List<Question>();
+			List<Question> highDifficultyQuestions = new List<Question>();
 
-            gameQuestions.Add(SelectQuestionByDifficulty(lowDifficultyQuestions,gameCategories));
-            gameQuestions.Add(SelectQuestionByDifficulty(mediumDifficultyQuestions, gameCategories));
-            gameQuestions.Add(SelectQuestionByDifficulty(highDifficultyQuestions, gameCategories));
+			List<Question> gameQuestions = new List<Question>();
 
-            SelectQuestionByCategory(gameCategories,gameQuestions);
+			foreach (var auxCategory in gameCategories)
+			{
+				lowDifficultyQuestions.AddRange(auxCategory.Questions.Where(a => a.QuestionDifficulty == 1));
+				mediumDifficultyQuestions.AddRange(auxCategory.Questions.Where(a => a.QuestionDifficulty == 2));
+				highDifficultyQuestions.AddRange(auxCategory.Questions.Where(a => a.QuestionDifficulty == 3));
+			}
 
-            gameQuestions.ForEach(a => a.Category = null);
-            return gameQuestions;
-        }
+			gameQuestions.Add(SelectQuestionByDifficulty(lowDifficultyQuestions));
+			gameQuestions.Add(SelectQuestionByDifficulty(mediumDifficultyQuestions));
+			gameQuestions.Add(SelectQuestionByDifficulty(highDifficultyQuestions));
 
-        public Question SelectQuestionByDifficulty(List<List<Question>> list, List<CategoryGame> gameCategories)
-        {
-            Random rnd = new Random();
-            var categoryQuestions = list.ElementAt(rnd.Next(0, gameCategories.Count));
-            var numberOfQuestions = categoryQuestions.Count;
-            return categoryQuestions.ElementAt(rnd.Next(0, numberOfQuestions));
-        }
+			SelectQuestionByCategory(gameCategories, gameQuestions);
 
-        public void SelectQuestionByCategory(List<CategoryGame> gameCategories, List<Question> gameQuestions)
-        {
-            Random rnd = new Random();
-            int i = 0;
-            while (i < 7)
-            {
-                foreach (var c in gameCategories)
-                {
-                    if (i < 7)
-                    {
-                        var category = _context.Categories.Include(a => a.Questions).Where(a => a.CategoryId == c.CategoryId).FirstOrDefault();
-                        var questions = category.Questions;
+			gameQuestions.ForEach(a => a.Category = null);
+			return gameQuestions;
+		}
 
-                        var questionToBeAdded = questions.ElementAt(rnd.Next(0, questions.Count));
-                        while (gameQuestions.Contains(questionToBeAdded))
-                        {
-                            questionToBeAdded = questions.ElementAt(rnd.Next(0, questions.Count));
-                        }
-                        gameQuestions.Add(questionToBeAdded);
-                        i++;
-                    }
-                }
-            }
-        }
-    }
+		public Question SelectQuestionByDifficulty(List<Question> list)
+		{
+			Random rnd = new Random();
+			return list.ElementAt(rnd.Next(0, list.Count));
+		}
+
+		public void SelectQuestionByCategory(List<Category> gameCategories, List<Question> gameQuestions)
+		{
+			Random rnd = new Random();
+			int i = 0;
+			while (i < 7)
+			{
+				foreach (var category in gameCategories)
+				{
+					if (i >= 7)
+					{
+						break;
+					}
+
+					var questionToBeAdded = category.Questions.ElementAt(rnd.Next(0, category.Questions.Count));
+					while (gameQuestions.Contains(questionToBeAdded))
+					{
+						questionToBeAdded = category.Questions.ElementAt(rnd.Next(0, category.Questions.Count));
+					}
+					gameQuestions.Add(questionToBeAdded);
+
+					i++;
+
+				}
+			}
+		}
+	}
 }
